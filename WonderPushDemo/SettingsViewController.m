48f908a0ee16a140c4d4bcf30cb4eee8e04c44be
Olivier Favre <olivier@wonderpush.com>
2016-06-27 11:03:42 +0200

    User id and notification flags

--- WonderPushDemo/SettingsViewController.m
+++ WonderPushDemo/SettingsViewController.m
@@ -10,10 +10,15 @@
 #import "AppDelegate.h"
 #import <WonderPush/WonderPush.h>
 
-@interface SettingsViewController ()
+@interface SettingsViewController () <UITextFieldDelegate>
 
-@property (nonatomic) IBOutlet id swtEnableNotifications;
-@property (nonatomic) IBOutlet id swtEnableGeolocation;
+@property (nonatomic) IBOutlet UISwitch *swtEnableNotifications;
+@property (nonatomic) IBOutlet UISwitch *swtEnableGeolocation;
+@property (nonatomic) IBOutlet UITextField *txtUserId;
+
+@property (nonatomic) IBOutlet UISwitch *swtNotificationTypeAlert;
+@property (nonatomic) IBOutlet UISwitch *swtNotificationTypeBadge;
+@property (nonatomic) IBOutlet UISwitch *swtNotificationTypeSound;
 
 @end
 
@@ -36,11 +41,25 @@
             [self loadSettings];
         }];
     }
+
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(currentUserNotificationSettings)]) {
+        UIUserNotificationSettings *currentSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+        UIUserNotificationType types = currentSettings ? currentSettings.types : 0;
+        self.swtNotificationTypeAlert.on = (types & UIUserNotificationTypeAlert);
+        self.swtNotificationTypeBadge.on = (types & UIUserNotificationTypeBadge);
+        self.swtNotificationTypeSound.on = (types & UIUserNotificationTypeSound);
+    } else {
+        UIRemoteNotificationType types = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+        self.swtNotificationTypeAlert.on = (types & UIRemoteNotificationTypeAlert);
+        self.swtNotificationTypeBadge.on = (types & UIRemoteNotificationTypeBadge);
+        self.swtNotificationTypeSound.on = (types & UIRemoteNotificationTypeSound);
+    }
 }
 
 - (void) loadSettings
 {
     [self.swtEnableNotifications setOn:[WonderPush getNotificationEnabled]];
+    self.txtUserId.text = [WonderPush userId];
 }
 
 - (IBAction) swtEnableNotifications_valueChange:(id)sender
@@ -57,6 +76,46 @@
     } else {
         [locationManager stopUpdatingLocation];
     }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField == self.txtUserId) {
+        NSLog(@"Setting userId to \"%@\"", self.txtUserId.text);
+        [WonderPush setUserId:self.txtUserId.text];
+    }
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (IBAction) swtNotificationType_valueChange:(UISwitch *)sender
+{
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationSettings *currentSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+        UIUserNotificationType types = currentSettings.types;
+        if (self.swtNotificationTypeAlert.isOn) types |= UIUserNotificationTypeAlert;
+        else types &= ~(UIUserNotificationTypeAlert);
+        if (self.swtNotificationTypeBadge.isOn) types |= UIUserNotificationTypeBadge;
+        else types &= ~(UIUserNotificationTypeBadge);
+        if (self.swtNotificationTypeSound.isOn) types |= UIUserNotificationTypeSound;
+        else types &= ~(UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:currentSettings.categories];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    } else {
+        UIRemoteNotificationType types = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+        if (self.swtNotificationTypeAlert.isOn) types |= UIRemoteNotificationTypeAlert;
+        else types &= ~(UIUserNotificationTypeAlert);
+        if (self.swtNotificationTypeBadge.isOn) types |= UIRemoteNotificationTypeBadge;
+        else types &= ~(UIUserNotificationTypeBadge);
+        if (self.swtNotificationTypeSound.isOn) types |= UIRemoteNotificationTypeSound;
+        else types &= ~(UIRemoteNotificationTypeSound);
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    }
+}
+
+- (IBAction) btnUnregister_touchUpInside:(UIButton *)sender
+{
+    [[UIApplication sharedApplication] unregisterForRemoteNotifications];
 }
 
 @end
